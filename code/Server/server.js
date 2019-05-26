d10dd6a329814221ede776c8045bfae456da3d02
smoothwood <ycm@outlook.com>
2019-05-26 16:00:47 +0800

    Add files via upload

--- code/Server/server.js
+++ code/Server/server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
  
-var preSignedUrl = v4.generate_signv4_mqtt("a3f3ep261pa8dz-ats.iot.ap-northeast-1.amazonaws.com", "ap-northeast-1", "", "");
+var preSignedUrl = v4.generate_signv4_mqtt("a3f3ep261pa8dz-ats.iot.ap-northeast-1.amazonaws.com", "ap-northeast-1", "AKIAZQKM55U7PUIIUHOX", "AU/VAHPphEiFNl3+0Q5Iil0Fa3lyNE9wrCXzMoSJ");
  
 port = 443
  
@@ -42,4 +42,5 @@ app.post('/command', function (req, res) {
     var topic = req.body.topic;
     var message = req.body.message;
     client.publish(topic, message);
+    res.end();
 });
